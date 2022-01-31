6a01c49f27e0a2926b98c466ef7de58b4f0c384f
Ayomide <ayomidedans@gmail.com>
2022-01-31 02:13:59 +0100

    sessions

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,6 @@
+from decimal import Decimal
+
+from store.models import Product
 
 
 class Basket():
@@ -10,5 +13,56 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number': 1234567}
+            basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(qty)}
+
+        # self.save()
+        self.session.modified = True
+
+    def __iter__(self):
+        """
+        Collect the product_id in the session data to
+        query the database and return products
+        """
+        product_ids = self.basket.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        basket = self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
+        """
+        Get the basket and count the quantity of items
+        """
+        return sum(item['qty'] for item in self.basket.values())
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())
+
+    def delete(self, product):
+        """
+        Delete item from session data
+        """
+        product_id = str(product)
+
+        if product_id in self.basket:
+            del self.basket[product_id]
+            # self.save()
+            self.session.modified = True
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,33 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from store.models import Product
+
+from .basket import Basket
+
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    print(basket)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+
+        return response
+
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
